8635b0375a3782713524d414bf23a26c62037164
AnnaKorr <7319162@gmail.com>
2017-03-03 14:41:01 +0300

    Add files via upload

---
+++ hw2-1/hw2-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hw2-1
+//
+//  Created by apple on 03.03.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
