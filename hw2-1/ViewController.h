bc1e294e27364b3c6c7732f2e328c8cedb0bbd84
AnnaKorr <7319162@gmail.com>
2017-03-03 14:29:01 +0300

    Initial Commit

---
+++ hw2-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hw2-1
+//
+//  Created by apple on 03.03.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hw2-1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hw2-1
+//
+//  Created by apple on 03.03.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
